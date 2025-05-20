241d42f1ae98831f8e46c1829b22bd219b6182fc
Luiz Martins <luizmartins.uemg@gmail.com>
2025-05-19 22:10:12 -0300

    Fix car price predictor project

---
+++ used-car-price-predictor/src/data_preprocessing.py
@@ -0,0 +1,21 @@
+class CleanerData: 
+    
+    def remove_outliers_with_specified_value(df,  column, value): 
+        return df[df[column] != value]
+    
+    @staticmethod
+    def remove_outliers_with_min_max(df,  column, lower_bound, upper_bound): 
+        return df[(df[column] >= lower_bound) & (df[column] <= upper_bound)]
+        
+    
+    @staticmethod
+    def fill_missing_values(df,  column, method='mean'):
+        if method == 'mean':
+            df[column].fillna(df[column].mean(), inplace=True)
+        elif method == 'median':
+            df[column].fillna(df[column].median(), inplace=True)
+        elif method == 'mode':
+            df[column].fillna(df[column].mode()[0], inplace=True)
+        
+        return df
+    
